89956d4548ec9b0b9c4d64b7743f94d4ec9d47c3
jdhao <jdhao@hotmail.com>
2022-05-15 21:16:02 +0800

    refactor: local fn = vim.fn

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,3 +1,4 @@
+local fn = vim.fn
 local api = vim.api
 local lsp = vim.lsp
 
@@ -188,10 +189,10 @@ else
 end
 
 -- Change diagnostic signs.
-vim.fn.sign_define("DiagnosticSignError", { text = "✗", texthl = "DiagnosticSignError" })
-vim.fn.sign_define("DiagnosticSignWarn", { text = "!", texthl = "DiagnosticSignWarn" })
-vim.fn.sign_define("DiagnosticSignInformation", { text = "", texthl = "DiagnosticSignInfo" })
-vim.fn.sign_define("DiagnosticSignHint", { text = "", texthl = "DiagnosticSignHint" })
+fn.sign_define("DiagnosticSignError", { text = "✗", texthl = "DiagnosticSignError" })
+fn.sign_define("DiagnosticSignWarn", { text = "!", texthl = "DiagnosticSignWarn" })
+fn.sign_define("DiagnosticSignInformation", { text = "", texthl = "DiagnosticSignInfo" })
+fn.sign_define("DiagnosticSignHint", { text = "", texthl = "DiagnosticSignHint" })
 
 -- global config for diagnostic
 vim.diagnostic.config({
